eb5599164e147ae2a956b8679fa89c756291c73f
Virgilio Gonzenbach <vjgonzenbach@gmail.com>
2023-02-22 15:42:33 -0500

    document oligo dataset

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Oligodendrocyte array
+#'
+#'  The oligo dataset in bossrdata consists of a small 4D region-of-interest in cortex from an adult transgenic mouse
+#'  expressing EGFP in the cytoplasm of oligodendrocytes (MOBP-EGFP). This same region was imaged at least weekly using two-photon 
+#'  imaging through a cranial window over the course of cuprizone-treatment and recovery.
+#'
+"oligo"
+
